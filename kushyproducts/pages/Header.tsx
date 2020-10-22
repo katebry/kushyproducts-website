d1d98fbfdcb911752a06dc294adaf840e1506c76
Kate Bryan <katebryan31@gmail.com>
2020-10-22 11:08:33 +0100

    chore: playing around with button styles in Header

--- kushyproducts/pages/Header.tsx
+++ kushyproducts/pages/Header.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-const ContactUsWrapper = styled.div`
+const ContactUsWrapper = styled.button`
+  border: 0;
+  background-color: white;
   padding-top: 25px;
   grid-area: 1 / 2 / 2 / 4;
   display: flex;
@@ -12,9 +14,17 @@ const ContactUsWrapper = styled.div`
   @media only screen and (max-width: 426px) {
     font-size: 14px;
   }
+  :hover,
+  :active, 
+  :focus {
+    background: #79C098;
+    border: 0;
+}
 `;
 
-const AboutUsWrapper = styled.div`
+const AboutUsWrapper = styled.button`
+  border: 0;
+  background-color: white;
   padding-top: 25px;
   grid-area: 1 / 4 / 2 / 6;
   display: flex;
@@ -26,9 +36,17 @@ const AboutUsWrapper = styled.div`
   @media only screen and (max-width: 426px) {
     font-size: 14px;
   }
+  :hover,
+  :active, 
+  :focus {
+    background: #79C098;
+    border: 0;
+}
 `;
 
-const EventsWrapper = styled.div`
+const EventsWrapper = styled.button`
+  border: 0;
+  background-color: white;
   padding-top: 25px;
   grid-area: 1 / 6 / 2 / 8;
   display: flex;
@@ -40,6 +58,12 @@ const EventsWrapper = styled.div`
   @media only screen and (max-width: 426px) {
     font-size: 14px;
   }
+  :hover,
+  :active, 
+  :focus {
+    background: #79C098;
+    border: 0;
+}
 `;
 
 export default function Header() {
