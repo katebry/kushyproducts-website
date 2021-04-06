5a683919816db0f88c4f9f2966491a4e04fea2be
Kate Bryan <katebryan31@gmail.com>
2021-04-06 20:54:03 +0100

    fix: added logo to main site page

--- kushyproducts/pages/index.tsx
+++ kushyproducts/pages/index.tsx
@@ -1,17 +1,12 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import MainContent from './MainContent'
-import { content } from '../assets/text'
 
 export default function Home() {
-
     return (
         <div className={styles.container}>
-            <Head>
-                <title>Kushy Products</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <MainContent content={content} />
+            <div className={styles.mainContent}>
+                <MainContent />
+            </div>
         </div>
     )
 }
