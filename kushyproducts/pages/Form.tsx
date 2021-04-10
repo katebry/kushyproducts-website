3988a3bd27c51829a62460be3604d52a6c40b8a5
Kate Bryan <katebryan31@gmail.com>
2021-04-10 08:08:45 +0100

    feat: added Form inpputs and styling

--- kushyproducts/pages/Form.tsx
+++ kushyproducts/pages/Form.tsx
@@ -19,28 +19,55 @@ const FormGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr repeat(2, 3fr) 1fr;
   grid-template-rows: 4fr;
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
 `;
 
 const Input = styled.input`
+  grid-area: 1 / 2 / 2 / 3;
   background-color: white;
   border: none;
   border-radius: 0px;
   color: black;
+  min-height: 8vh;
 
   &:focus {
     outline: none;
   }
 `;
 
+const EmailInput = styled(Input)`
+  grid-area: 1 / 3 / 2 / 4;
+`;
+
+const TextInput = styled(Input)`
+  grid-area: 2 / 2 / 4 / 4;
+  min-height: 20vh;
+  padding-bottom: 20px;
+`;
+
+const BottomSection = styled.div`
+  grid-area: 4 / 1 / 5 / 5;
+  background-color: #e8461c;
+  min-height: 8vh;
+`;
+
+const FormWrapper = styled.div`
+  margin-bottom: 30px;
+`;
+
 export default function Form() {
   return (
-    <FormContainer id="contact">
-      <Title>GET IN TOUCH</Title>
-      <FormGrid>
-        <Input />
-      </FormGrid>
-    </FormContainer>
+    <FormWrapper>
+      <FormContainer id="contact">
+        <Title>GET IN TOUCH</Title>
+        <FormGrid>
+          <Input />
+          <EmailInput />
+          <TextInput />
+          <BottomSection />
+        </FormGrid>
+      </FormContainer>
+    </FormWrapper>
   );
 }
