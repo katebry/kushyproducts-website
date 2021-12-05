d004f1c2b36dcce5e9865f59280cc6d4c7af0e7f
katebry <katebryan31@gmail.com>
2021-12-05 15:58:55 -0800

    chore: using next/image for bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import HeaderButtons from "./HeaderButtons";
 import MainContent from "./MainContent";
 import InfoBar from "./InfoBar";
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <div className="container">
+      <Image
+        alt="Psychadelic colours"
+        src="/BackgroundMain.png"
+        layout="fill"
+        priority={true}
+        quality={100}
+      />
         <div className="buttons">
           <HeaderButtons />
         </div>
